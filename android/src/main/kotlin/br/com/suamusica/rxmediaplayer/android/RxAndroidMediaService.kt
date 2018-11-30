88676fd7cd6059563a799fede627a53faac61d9b
Alan Lira <alanmlira@gmail.com>
2018-11-30 08:32:29 -0200

    solve notification update problems

--- android/src/main/kotlin/br/com/suamusica/rxmediaplayer/android/RxAndroidMediaService.kt
+++ android/src/main/kotlin/br/com/suamusica/rxmediaplayer/android/RxAndroidMediaService.kt
@@ -5,7 +5,6 @@ import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
 import android.media.AudioAttributes
 import android.media.AudioFocusRequest
 import android.media.AudioManager
@@ -13,24 +12,20 @@ import android.os.Binder
 import android.os.Build
 import android.os.IBinder
 import android.support.annotation.RequiresApi
-import android.support.v4.app.NotificationCompat
 import android.telephony.PhoneStateListener
 import android.telephony.TelephonyManager
+import android.util.Log
 import br.com.suamusica.rxmediaplayer.domain.CompletedState
 import br.com.suamusica.rxmediaplayer.domain.IdleState
 import br.com.suamusica.rxmediaplayer.domain.MediaBoundState
 import br.com.suamusica.rxmediaplayer.domain.MediaServiceState
-import br.com.suamusica.rxmediaplayer.domain.PausedState
-import br.com.suamusica.rxmediaplayer.domain.PlayingState
 import br.com.suamusica.rxmediaplayer.domain.RxMediaPlayer
 import br.com.suamusica.rxmediaplayer.domain.RxMediaService
 import br.com.suamusica.rxmediaplayer.domain.StoppedState
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.SimpleTarget
-import com.bumptech.glide.request.transition.Transition
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposables
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
 
 
 abstract class RxAndroidMediaService : Service() {
@@ -38,7 +33,7 @@ abstract class RxAndroidMediaService : Service() {
   private val audioManager by lazy { getSystemService(Context.AUDIO_SERVICE) as AudioManager }
 
   private val binder = LocalBinder()
-  private var disposable = Disposables.disposed()
+  private var disposable = CompositeDisposable()
 
   private lateinit var phoneStateListener: PhoneStateListener
   private lateinit var onAudioFocusChangeListener: AudioManager.OnAudioFocusChangeListener
@@ -52,12 +47,23 @@ abstract class RxAndroidMediaService : Service() {
     phoneStateListener = RxMediaServiceSystemListeners.CustomPhoneStateListener(rxMediaService)
     onAudioFocusChangeListener = RxMediaServiceSystemListeners.OnAudioFocusChangeListener(rxMediaService)
 
-    disposable = rxMediaService.stateChanges()
+    disposable.add(
+        rxMediaService.stateChanges()
         .observeOn(AndroidSchedulers.mainThread())
         .doOnNext { notify(it) }
         .doAfterTerminate { removeNotification() }
         .doAfterTerminate { rxMediaService.release() }
         .subscribe()
+    )
+
+    disposable.add(
+        observeNotification()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext { bindNotification(it) }
+            .doOnDispose { removeNotification() }
+            .subscribe()
+    )
   }
 
   override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -80,9 +86,12 @@ abstract class RxAndroidMediaService : Service() {
 
   abstract fun createRxMediaPlayer(): RxMediaPlayer
 
-  abstract fun createNotification(state: MediaBoundState): NotificationCompat.Builder
+  abstract fun createNotification(state: MediaBoundState)
+
+  abstract fun observeNotification(): PublishSubject<Notification>
 
   protected open fun removeNotification() {
+    Log.d("RxMediaService", "removeNotification()")
     stopForeground(true)
     notificationManager.cancel(NOTIFICATION_ID)
     telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_NONE)
@@ -90,12 +99,13 @@ abstract class RxAndroidMediaService : Service() {
 
   private fun notify(state: MediaServiceState) {
     when (state) {
-      is CompletedState, is IdleState -> removeNotification()
+      is CompletedState, is IdleState, is StoppedState -> removeNotification()
       else -> showNotification(state as MediaBoundState)
     }
   }
 
   private fun showNotification(state: MediaBoundState) {
+    Log.d("RxMediaService", "showNotification()")
     val result = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
       requestAudioFocusForAndroidO()
     } else {
@@ -104,18 +114,13 @@ abstract class RxAndroidMediaService : Service() {
 
     if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
       // Start playback
-      val notificationBuilder = createNotification(state)
-
-      Glide.with(this)
-          .asBitmap()
-          .load(state .item?.coverUrl)
-          .into(object : SimpleTarget<Bitmap>() {
-            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?){
-              notificationBuilder.setLargeIcon(resource)
-              showNotificationOnScreen(notificationBuilder.build())
-            }
-          })
+      createNotification(state)
     }
+  }
+
+  private fun bindNotification(notification: Notification) {
+    notificationManager.notify(NOTIFICATION_ID, notification)
+    showNotificationOnScreen(notification)
   }
 
   fun showNotificationOnScreen(notification: Notification) {
@@ -145,7 +150,7 @@ abstract class RxAndroidMediaService : Service() {
   }
 
   companion object {
-    const val NOTIFICATION_ID = 1342134
+    const val NOTIFICATION_ID = 0x0a0c
     lateinit var rxMediaService: RxMediaService
   }
 
